33370f8d104d3b69ce4c689783818512e7f864f2
Jan Kumor <akumor@gmail.com>
2016-09-03 04:02:42 -0400

    Updated test to try and test the masking.

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,15 @@
-#!/usr/bin/env/python
+#!/usr/bin/env python
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
-import rastervectoreanalysis
 import unittest
+sys.path.insert(0, os.path.abspath('..'))
+from rastervectoreanalysis import RasterVectorAnalysis
+import numpy as np
+from osgeo import gdal
+from osgeo import gdalconst
 
 
-class TestRundeckCalendar(unittest.TestCase):
+class TestRasterVectorAnalysis(unittest.TestCase):
     """
     Tests the method used to parse the CSV file and update the AppDeploymentListFile object.
     """
@@ -23,8 +26,18 @@ class TestRundeckCalendar(unittest.TestCase):
         """
         pass
 
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_plot_tif(self):
+        """ Tests plot_tif function of the RasterVectorAnalysis class. """
+        tif_path = os.path.dirname(os.path.realpath(__file__)) + '/test_data/chesapeaketry_1.tif'
+        RasterVectorAnalysis.plot_tif(tif_path, cheap_cloud_mask=None)
+        # Create numpy array to use for the cheap_cloud_mask
+        source = gdal.Open(tif_path, gdalconst.GA_ReadOnly).ReadAsArray()
+        mask = np.zeros(source.shape)
+        for (x, y), value in np.ndenumerate(source):
+            if source[x, y] > 30000:
+                mask[x, y] = 1
+        source = None
+        RasterVectorAnalysis.plot_tif(tif_path, cheap_cloud_mask=mask)
 
 
 if __name__ == '__main__':
